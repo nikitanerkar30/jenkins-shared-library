64b3408d699bae8e562c70dec64f49ecbc1ea40d
Nikita Nerkar <nikita.c.nerkar@gmail.com>
2026-08-06 18:45:40 +0530

    Add parameters

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call(String usernameCredentialId, String passwordCredentialId){
 
 
 withCredentials([
--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call(String imageName, String imageTag){
 
 
 retry(3){
